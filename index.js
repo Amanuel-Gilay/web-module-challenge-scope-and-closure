a8076e0b31ab625d89fea2e19a29601b84ab28f6
Amanuel Gilay <amanuelgilay@Amanuels-MacBook-Pro.local>
2021-03-22 22:25:47 -0700

    soved project scoop and closure

--- index.js
+++ index.js
@@ -28,11 +28,14 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+  counter1 is local scope whereas counter2 is global scope.
   
   2. Which of the two uses a closure? How can you tell?
+  counter1 uses closuere, becase it's nested in the same gate.
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+     counter1 would be preferable in game score senario.
 */
 
 // counter1 code
@@ -62,10 +65,12 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
+function inning(){
     /*Code Here*/
+    return Math.floor(Math.random() * (2));
 }
 
+console.log(inning());
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
@@ -81,9 +86,27 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(inning,score){
   /*Code Here*/
-}
+  const totalscore = [];
+  let homeScore = 0;
+  let awayScore = 0;
+
+  for(let i = 0; i < score; i++){
+    //const currentScore = inning();
+    homeScore = homeScore + inning()
+    awayScore = awayScore + inning()
+
+  }
+
+  return {
+    'home': homeScore,
+    'away': awayScore
+  };
+  }
+    
+
+console.log(finalScore(inning,7));
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
